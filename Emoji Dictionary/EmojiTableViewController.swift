fdab1bc5378497d5acfff3c92cb450e0d1e19858
Rico Alderete <su8lim3@gmail.com>
2019-04-21 20:26:28 -0600

    Sending the correct emoji to the view controller

--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -32,14 +32,15 @@ class EmojiTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        performSegue(withIdentifier: "ourSegue", sender: nil)
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "ourSegue", sender: emoji)
         
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let emojiDefVC = segue.destination as! EmojiDefinitionViewController
-        emojiDefVC.emoji = "We Did It!"
+        emojiDefVC.emoji = sender as! String
         
     }
     
